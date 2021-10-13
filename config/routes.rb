d3c3654211f60f99b3ed313967429d94725fcaf5
kazuya120 <songlinheye2@gmail.com>
2021-10-13 06:44:52 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'relationships#followinngs', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+end   
+   
   resources :books,only: [:show,:index,:create,:edit,:update,:destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
