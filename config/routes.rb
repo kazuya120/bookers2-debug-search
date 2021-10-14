12809dcc6e29ab0d4034e978a4edc1f1c26b6141
kazuya120 <songlinheye2@gmail.com>
2021-10-14 02:57:40 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  get '/search' => 'searches#search'
   
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships,only: [:create, :destroy]
